afa003cf79efdc93a6bcaf8c4d624bae780462d4
jiangenhe <jiangenhe@gmail.com>
2023-03-23 15:34:15 -0400

    Reusable Component - Comment update data code

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ let enrollment = [
 const chart = new Chart({width: 500, height: 200}, 'body', enrollment)
 chart.updateVis()
 
-
-let enrollment_new = [
-  {year: "2017", enroll: 28321},
-  {year: "2018", enroll: 28894},
-  {year: "2019", enroll: 29460}
-]
-chart.data = enrollment_new
-chart.updateVis()
+//
+// let enrollment_new = [
+//   {year: "2017", enroll: 28321},
+//   {year: "2018", enroll: 28894},
+//   {year: "2019", enroll: 29460}
+// ]
+// chart.data = enrollment_new
+// chart.updateVis()
